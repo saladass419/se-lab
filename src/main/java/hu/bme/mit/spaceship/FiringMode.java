9e886ca1d50cdb4ee368bc863147d4448b189c10
unknown <168741@BME.HU>
2023-10-04 08:46:21 +0200

    analysis setup

--- src/main/java/hu/bme/mit/spaceship/FiringMode.java
+++ src/main/java/hu/bme/mit/spaceship/FiringMode.java
@@ -3,7 +3,7 @@ package hu.bme.mit.spaceship;
 /**
 * Weapon firing mode enumeration
 *
-*/
+*/ 
 public enum FiringMode {
   SINGLE, ALL
 }
--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,6 +79,7 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
+        
         if(!primaryTorpedoStore.isEmpty()&&!secondaryTorpedoStore.isEmpty()){
           if(primaryTorpedoStore.fire(1)){
             if(secondaryTorpedoStore.fire(1)){
